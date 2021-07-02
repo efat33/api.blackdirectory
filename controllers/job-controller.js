42709900e9654055abd8be3c1b96a0a5725c9385
Zakaria Ahmed <zakaria.buet13@gmail.com>
2021-07-02 20:24:26 +0600

    Create new job API, get job API, job file upload

--- controllers/job-controller.js
+++ controllers/job-controller.js
@@ -8,22 +8,43 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 class JobController {
-
-  
-
   sectors = async (req, res, next) => {
-    
     const sectors = await JobModel.getSectors();
 
-    new AppSuccess(res, 200, "200_detailFound", {'entity': 'entity_sectors'}, sectors );
- 
+    new AppSuccess(res, 200, "200_detailFound", { 'entity': 'entity_sectors' }, sectors);
   }
 
+  newJob = async (req, res, next) => {
+    const job = await JobModel.createJob(req.body, req.currentUser);
 
+    new AppSuccess(res, 200, "200_added", { 'entity': 'entity_job' }, job);
+  }
 
-  
+  getJobs = async (req, res, next) => {
+    const result = await JobModel.getJobs();
 
+    if (Object.keys(result).length === 0) {
+      throw new AppError(403, "403_unknownError")
+    };
 
+    new AppSuccess(res, 200, "200_detailFound", { 'entity': 'entity_job' }, result);
+  };
+
+  getUserJobs = async (req, res, next) => {
+    const result = await JobModel.getJobs({ 'Job.user_id': req.currentUser.id });
+
+    if (Object.keys(result).length === 0) {
+      throw new AppError(403, "403_unknownError")
+    };
+
+    result.forEach(job => {
+      job.featured = !!job.featured;
+      job.filled = !!job.filled;
+    });
+
+    new AppSuccess(res, 200, "200_detailFound", { 'entity': 'entity_job' }, result);
+  };
 }
 
 module.exports = new JobController();
+
--- routes/jobs-routes.js
+++ routes/jobs-routes.js
@@ -5,12 +5,13 @@ const auth = require('../utils/auth');
 const authVerified = require('../utils/authVerified');
 const apiKey = require('../utils/api-key');
 const awaitHandlerFactory = require('../utils/awaitHandlerFactory');
+const { isEmployer } = require('../utils/common');
 
 const validation = require('../utils/listingValidator');
 
 
 router.get('/sectors', apiKey(), awaitHandlerFactory(jobController.sectors));
-
-
+router.post('/new-job', apiKey(), auth(), isEmployer(), awaitHandlerFactory(jobController.newJob));
+router.get('/get-user-jobs', apiKey(), auth(), isEmployer(), awaitHandlerFactory(jobController.getUserJobs));
 
 module.exports = router;
