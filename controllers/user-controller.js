5a93ab07523a8fd3d3dd9cdd2c576f3bcbd6d2b4
Efat Ahmed <efat.ctet@gmail.com>
2022-02-23 12:43:56 +0600

    remove returning password from check authentication

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -533,7 +533,12 @@ class UserController {
   checkAuthentication = async (req, res, next) => {
 
     let user = '';
-    if (req.currentUser) user = await UserModel.findOne({ 'id': req.currentUser.id });
+    if (req.currentUser){
+      user = await UserModel.findOne({ 'id': req.currentUser.id });
+      const { password, ...userWithoutPassword } = user;
+  
+      new AppSuccess(res, 200, "200_successful", "", userWithoutPassword);
+    } 
 
     new AppSuccess(res, 200, "200_successful", "", user);
 
